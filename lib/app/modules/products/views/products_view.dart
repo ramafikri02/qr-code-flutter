ec9ce8efb25e341f4415131fc20e4fe10939eae9
ramafikri02 <ramafikri02@gmail.com>
2024-05-01 19:37:04 +0700

    ✨ feat(list product): work

--- lib/app/modules/products/views/products_view.dart
+++ lib/app/modules/products/views/products_view.dart
@@ -1,6 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import 'package:get/get.dart';
+import 'package:qr_code/app/data/models/product_model.dart';
+import 'package:qr_code/app/routes/app_pages.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 import '../controllers/products_controller.dart';
 
@@ -9,16 +15,77 @@ class ProductsView extends GetView<ProductsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ProductsView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'ProductsView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('List Products'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            stream: controller.streamProducts(),
+            builder: (context, snapProducts) {
+              if (snapProducts.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              if (snapProducts.data!.docs.isEmpty) {
+                return const Center(
+                  child: Text("No products"),
+                );
+              }
+
+              List<ProductModel> allProducts = [];
+
+              for (var element in snapProducts.data!.docs) {
+                allProducts.add(ProductModel.fromJson(element.data()));
+              }
+              return ListView.builder(
+                  itemCount: allProducts.length,
+                  padding: const EdgeInsets.all(20),
+                  itemBuilder: (context, index) {
+                    ProductModel product = allProducts[index];
+                    return Card(
+                      elevation: 5,
+                      margin: const EdgeInsets.only(bottom: 20),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(9)),
+                      child: InkWell(
+                        onTap: () {
+                          Get.toNamed(Routes.detailProduct, arguments: product);
+                        },
+                        borderRadius: BorderRadius.circular(9),
+                        child: Container(
+                          height: 100,
+                          padding: const EdgeInsets.all(20),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      product.code,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Text(product.name),
+                                    Text("jumlah : ${product.qty}")
+                                  ],
+                                ),
+                              ),
+                              SizedBox(
+                                height: 50,
+                                width: 50,
+                                child: QrImageView(
+                                    data: product.code,
+                                    size: 200.0,
+                                    version: QrVersions.auto),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            }));
   }
 }
